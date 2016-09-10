0b30f9e0ddfe3f8a6cffce25f3ffd36fe196502c
Joshua Webb <bbew.auhsoj+git@gmail.com>
2016-09-11 01:33:16 +1000

    showSaverMode
     - Doesn't work... I think sony have their own Stamina Mode
       "SuperStamina" => /system/priv-app/SuperStamina/SuperStamina.apk
       com.sonymobile.superstamina

--- app/src/main/java/io/github/joshuawebb/nostaminanotification/NoStaminaNotification.java
+++ app/src/main/java/io/github/joshuawebb/nostaminanotification/NoStaminaNotification.java
@@ -1,8 +1,5 @@
 package io.github.joshuawebb.nostaminanotification;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -23,83 +20,19 @@ public class NoStaminaNotification implements IXposedHookLoadPackage {
 		String targetClassName = targetPackage + ".power.PowerNotificationWarnings";
 		final Class<?> notifierClass = XposedHelpers.findClass(targetClassName, lpparam.classLoader);
 
-		for (Field f : notifierClass.getDeclaredFields()) {
-			XposedBridge.log("Field: " + f.getName() + " | " + Modifier.isStatic(f.getModifiers()) + " | " + f.isAccessible());
-		}
-
-		final Field notificationManager = findField(notifierClass, "mNoMan");
-		if (notificationManager == null) {
-			XposedBridge.log("notification manager not found");
-			return;
-		}
-
-		final Field showingStringsField = findField(notifierClass, "SHOWING_STRINGS");
-		if (showingStringsField == null) {
-			XposedBridge.log("showingStrings not found");
-			return;
-		}
-
-		notificationManager.setAccessible(true);
-
-		showingStringsField.setAccessible(true);
-		String[] showingStrings = (String[])showingStringsField.get(null);
-		for(String s : showingStrings) {
-			XposedBridge.log("SHOWING_STRING: " + s);
-		}
-
-		XC_MethodHook blockCallHook = new XC_MethodHook() {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				super.beforeHookedMethod(param);
-				XposedBridge.log(param.method.getName() + ": before hook!");
-				param.setResult(null);
-			}
-		};
-
-		for(String method : new String[] {
-			"showCriticalNotification",
-			"showWarningNotification",
-			"showInvalidChargerNotification",
-		})
-		{
-			try {
-				XposedHelpers.findAndHookMethod(notifierClass, method, blockCallHook);
-			} catch (Throwable t) {
-				XposedBridge.log("findAndHook '"+ method +"' failed: " + t.getMessage() + t);
-			}
-		}
-
-		XposedHelpers.findAndHookMethod(notifierClass, "showSaverMode",
+		// set saver mode to be off according to the Notification Generator
+		//  ~ not the PowerManager.
+		XposedHelpers.findAndHookMethod(notifierClass, "showSaverMode", "boolean",
 			new XC_MethodHook() {
 				@Override
 				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 					super.beforeHookedMethod(param);
 					XposedBridge.log(param.method.getName() + " called");
-					// set saver mode to be off...
+
+					// boolean : mode
 					param.args[0] = false;
 				}
 			}
 		);
-	}
-
-	private Field findField(Class<?> clazz, String fieldName)
-	{
-		try {
-			Field notificationManager = clazz.getDeclaredField(fieldName);
-			XposedBridge.log("Direct lookup success");
-			return notificationManager;
-		}
-		catch (Throwable t) {
-			XposedBridge.log("Direct lookup failed");
-		}
-
-		for (Field f : clazz.getDeclaredFields()) {
-			if (fieldName.equals(f.getName())) {
-				XposedBridge.log("Indirect lookup succeeded");
-				return f;
-			}
-		}
-
-		return null;
 	}
 }
